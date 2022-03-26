debc6d2a4cbab16305d7e0c828b0ffc14c98dfe0
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-03-26 02:25:43 +0100

    on process

--- src/Component/view/MainHomePage.js
+++ src/Component/view/MainHomePage.js
@@ -3,11 +3,13 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getBooks } from '../../redux/action'
+import { Products } from '../Product/Products'
 
 
 export const MainHomePage = () => {
 
   const books = useSelector((state) => state.book.data)
+  const [isLoading, setIsLoading] = useState(false)
 
   const dispatch = useDispatch()
 
@@ -23,30 +25,10 @@ export const MainHomePage = () => {
          <section  id="products" className="container mt-5">
       <div className="row">
     {
-      books && books.map((book, i) =>(
+      books.map((book, i) =>(
         <>
         
-        <div key ={i._id}className="col-sm-12 col-md-6 col-lg-3 my-3">
-          <div className="card p-3 rounded">
-            <img
-              className="card-img-top mx-auto"
-              src={book.images[0].imgUrl}
-              />
-            <div className="card-body d-flex flex-column ">
-              <h5 className="card-title">
-                <a href="" className='ml-2'>{book.title}</a>
-              </h5>
-              <div className="ratings mt-auto">
-                <div className="rating-outer">
-                  <div className="rating-inner"></div>
-                </div>
-                <span id="no_of_reviews">({book.ratings} Reviews)</span>
-              </div>
-              <p className="card-text">${book.price}</p>
-              <a href="#" id="view_btn" className="btn btn-block rounded-pill">View Details</a>
-            </div>
-          </div>
-        </div>
+       <Products book= {book} key={i._id}/>
         </>
       ))
 }
