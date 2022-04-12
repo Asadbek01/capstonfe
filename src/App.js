8d938c4f8c11986139001cc605f1097b11252854
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-04-12 07:24:16 +0200

    working with searching

--- src/App.js
+++ src/App.js
@@ -6,30 +6,28 @@ import { MainHomePage } from './Component/BooksStore/MainHomePage';
 
 import { MyNavbar } from './Component/BooksStore/Navbar';
 import { CartPage } from './Component/BooksStore/CartPage';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container, Row } from 'react-bootstrap';
 import Login from './Component/view/LoginPage';
 import SignUp from './Component/view/SignUpPage';
 import BookDetail  from "./Component/BooksStore/BookDetail"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Footer } from './Component/view/Footer/Footer';
-import { SearchBooks } from './Component/view/SearchBooks';
 import { PageError } from './Component/view/PageError';
 function App() {
   return (
-  <BrowserRouter> 
+  <Router>
    <MyNavbar />
-   <Routes>
+     <Routes>
    <Route path='/login' element={<Login/>}/>
    <Route path='/signUp' element={<SignUp/>}/>
      <Route path='/' element={<MainHomePage/>} exact/>
-     {/* <Route path='/search/:SearchQuery' element={<MainHomePage/>}/> */}
      <Route path='/cart' element={<CartPage />}/>
      <Route path = "/detail/:id" element={<BookDetail />} exact />
      <Route path="*" element={<PageError />} />
-    </Routes>
+     </Routes>
    <Footer />
-  </BrowserRouter>
+  </Router>
     )
 }
 
